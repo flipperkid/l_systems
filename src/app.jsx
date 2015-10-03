2f582d62db92da10429c7026d3cf4b817d0ac73a
Chris Jordan <flipperkid@gmail.com>
2015-10-03 10:43:17 -0400

    Minor tweaks and improvements

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,15 @@
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Canvas from './Canvas.jsx';
 import UserInput from './UserInput.jsx';
 import store from './store';
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 class Root extends Component {
   render() {
